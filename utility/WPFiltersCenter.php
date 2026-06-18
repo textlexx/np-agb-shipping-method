591bc3df33524e5ff4406a2726c2fdd583fde370
sanyoksanya <textlexx@gmail.com>
2026-06-18 12:05:25 +0300

    save

--- utility/WPFiltersCenter.php
+++ utility/WPFiltersCenter.php
@@ -5,16 +5,19 @@ namespace NpAgbShippingMethod;
 class WPFiltersCenter{
 	
 	//-----------------------------------------------------------------------
-	/* Example
-	public static function addFilter_off_login_page_for_admin_panel(){
+	// Example
+	/*
+	public static function addFilterExample():?bool{
 		
 		// Return default value if not exists option
 		$return_default = 0;
 		
-		if(get_option('regLogAgb_off_standart_login_adm', $return_default) != 1) return false;
+		if(get_option('needed_option_example', $return_default) != 1) return false;
 		if(!preg_match('#^/wp-admin#', $_SERVER['REQUEST_URI'])) return false;
 		
-		add_filter('login_url', RegLogAgbInit::$root_page, 10);
+		add_filter('example_filter_name', ClassName::$example, 10);
+
+		return null;
 	}
 	*/
 	//-----------------------------------------------------------------------
--- utility/WPGetOptFunc.php
+++ utility/WPGetOptFunc.php
@@ -5,17 +5,19 @@ namespace NpAgbShippingMethod;
 class WPGetOptFunc{
 	
 	//-----------------------------------------------------------------------
-	/* Example
-	public static function login_page_for_admin_panel(){
+	// Example
+	/*
+	public static function login_page_for_admin_panel():string{
 		
 		// Return default value if not exists option
 		$return_default = '';
 		
-		$custom = get_option('regLogAgb_login_page_admin_panel', $return_default);
-		if($custom === '') return '/wp-login.php?reg_log_agb_login';
+		$custom = get_option('example_option_name', $return_default);
+		if($custom === '') return '/wp-login.php?example_param';
 		
 		return $custom;
 	}
 	*/
+	
 	//-----------------------------------------------------------------------
 }
